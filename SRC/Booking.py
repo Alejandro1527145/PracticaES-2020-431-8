59f168f5a2fa87b8248912c391d804f0775bf35b
Alejandro1527145 <alefut2000@hotmail.com>
2020-05-14 21:24:27 +0200

    Info de clase Booking

--- SRC/Booking.py
+++ SRC/Booking.py
@@ -3,6 +3,22 @@ from . import Hotels
 
 
 class Booking():
+    """
+    Encapsula l'accés a la plataforma Booking.com per gestionar allotjaments
+
+    El mètode confirm_reserve reb com a pàràmetres l'usuari que ha fet la pre-reserva i la llista d'hotels' que es volen reservar
+
+    Les classes 'User' i 'Hotels' són classes pròpies de airhopping que heu de definir
+    'Hotels' encapsula la API externa proporcionada per booking.com
+
+    La classe 'User' conté informació de l'usuari que fa la reserva
+    La classe 'Hotels' conté la llista d'hotels' pels quals es vol confirmar la reserva. De cada hotel té la següent informació:
+    - Codi de l'hotel
+    - Nom de l'hotel
+    - Número d'hostes
+    - Número d'habitacions
+    - Durada en dies de la reserva
+    """
 
     def __init__(self):
         pass
