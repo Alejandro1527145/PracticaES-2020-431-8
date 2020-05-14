6150e17a337a23a468ab4803ed06143c95785f70
Alejandro1527145 <alefut2000@hotmail.com>
2020-05-14 21:21:11 +0200

    Info de clase Bank

--- SRC/Bank.py
+++ SRC/Bank.py
@@ -3,7 +3,21 @@ from . import PaymentData
 
 
 class Bank:
+    """
+    Encapsula l'accés a la plataforma bancària per realitzar pagaments
 
+    El mètode do_payment reb com a pàràmetres l'usuari que fa el pagament i les dades del pagament
+
+    Les classes 'User' i 'PaymentData' són classes pròpies de airhopping que heu de definir
+
+    La classe 'User' conté informació de l'usuari que fa el pagament
+    La classe 'PaymentData' 'PaymentData' conté les dades necessàries per poder efectuar el pagament:
+    - Tipus de targeta : VISA o Mastercard
+    - Nom del titular (el que apareix a la targeta)
+    - Número de la targeta
+    - Codi de seguretat
+    - Import
+    """
     def __init__(self):
         pass
 
