aae2d00bb29f9b4a5fc7ab3a64f42527555f471b
Alejandro1527145 <alefut2000@hotmail.com>
2020-05-14 21:21:57 +0200

    Info de clase Skyscanner

--- SRC/Skyscanner.py
+++ SRC/Skyscanner.py
@@ -3,6 +3,20 @@ from . import Flights
 
 
 class Skyscanner():
+    """
+    Encapsula l'accés a la plataforma Skyscanner.com per gestionar vols
+
+    El mètode confirm_reserve reb com a pàràmetres l'usuari que ha fet la pre-reserva i la llista de vols que es volen reservar
+
+    Les classes 'User' i 'Flights' són classes pròpies de airhopping que heu de definir
+    'Flights' encapsula la API externa proporcionada per Skyscanner.com
+
+    La classe 'User' conté informació de l'usuari que fa la reserva
+    La classe 'Flights' conté la llista de vols pels quals es vol confirmar la reserva. De cada vol té la següent informació:
+    - Codi del vol
+    - Destinació
+    - Número de passatgers
+    """
 
     def __init__(self):
         pass
