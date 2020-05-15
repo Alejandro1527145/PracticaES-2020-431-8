3a99a3fd17537a324ccc12ba4696f863661f7903
domen <Domenec.Madrid@e-campus.uab.cat>
2020-05-15 08:36:17 +0200

    Class Flights

--- SRC/Flights.py
+++ SRC/Flights.py
@@ -1,4 +1,13 @@
 class Flights:
+    """
+    - Codi del vol
+    - Destinació
+    - Número de passatgers
 
-    def __init__(self):
-        pass
+   """
+    def __init__(self, codi_vol, destinacio, n_passatgers):
+        self.codi_vol = ""
+        self.destinacio = ""
+        self.n_passatgers = ""
+
+
