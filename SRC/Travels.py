53c31f3923708a50dbe66fff8f74049690df1f2d
1528230 <sergio.lopese@e-campus.uab.cat>
2020-05-17 10:56:10 +0200

    función para calcular precio viaje

--- SRC/Travels.py
+++ SRC/Travels.py
@@ -4,12 +4,23 @@ from . import User
 
 class Travels:
 
-    def __init__(self, flight, hotel, car):
+    def __init__(self, flights, hotels, cars, price, pasaj, orig, dateI, dateF):
+        self.pasaj = pasaj
+        self.orig = orig
+        self.flights = flights
+        self.hotels = hotels
+        self.cars = cars
+        self.dateI = dateI
+        self.dateF = dateF
+        self.price = price
 
-        self.car = car
-        self.flight = flight
-        self.hotel = hotel
-        self.price = 0
+    def calcularPrecio(self):
+        for hotel in self.hotels:
+            self.price = self.price + hotel.price
+        for flight in self.flights:
+            self.price = self.price + flight.price
+        for car in self.cars:
+            self.price = self.price + car.price
 
     def manage(self):
         pass
