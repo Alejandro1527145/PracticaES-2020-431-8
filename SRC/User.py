a1b13bdd8c1a871d6a993d41916bbf45d5adf257
1528230 <sergio.lopese@e-campus.uab.cat>
2020-05-17 19:47:15 +0200

    constructor con parámetros con la informacion del pago

--- SRC/User.py
+++ SRC/User.py
@@ -8,8 +8,15 @@ class User:
         Travels.reserva = []
         PaymentData.tarjeta
 
+    def __init__(self, name, DNI, telf, email, payment_data: PaymentData):
+        self.name = name
+        self.DNI = DNI
+        self.telf = telf
+        self.email = email
+        self.payment_data = payment_data
+
     def getName(self) -> str:
         return self.name
 
     def setName(self, name) -> None:
-        self.name = name;
+        self.name = name
