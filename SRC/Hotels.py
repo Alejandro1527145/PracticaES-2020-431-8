2baf38c9a479039550822c4f2133bfa3c65c754a
1528230 <sergio.lopese@e-campus.uab.cat>
2020-05-17 17:11:04 +0200

    constructor con todos los datos del hotel

--- SRC/Hotels.py
+++ SRC/Hotels.py
@@ -2,3 +2,11 @@ class Hotels:
 
     def __init__(self):
         pass
+
+    def __init__(self, nombre, dias, pers, habitaciones, id, precio):
+        self.id = id
+        self.nombre = nombre
+        self.pers = pers
+        self.habitaciones = habitaciones
+        self.dias = dias
+        self.precio = precio
